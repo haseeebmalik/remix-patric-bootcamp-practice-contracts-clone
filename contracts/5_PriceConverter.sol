d235db7daed626059778f9a4c2a08cb55db050de
haseeebmalik <m.haseebmalik123@gmail.com>
2023-05-28 10:27:34 +0500

    some updates

--- contracts/5_PriceConverter.sol
+++ contracts/5_PriceConverter.sol
@@ -1,6 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";       
 
 
 // a library cannot  have state variables and can send ethers
